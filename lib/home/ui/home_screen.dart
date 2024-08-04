1b3ca64b3d561d27faf961da8fac0273c5737708
Abhay Panwar <abhaypanwar@Abhays-MacBook-Air.local>
2024-08-04 14:00:52 +0530

    room images added with vertical room name

--- lib/home/ui/home_screen.dart
+++ lib/home/ui/home_screen.dart
@@ -1,10 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_ease/home/ui/page_indicator.dart';
 import 'package:home_ease/home/ui/rooms_page_view.dart';
-import 'package:home_ease/theme/colors.dart';
 import 'package:home_ease/home/cubit/home_cubit.dart';
 import 'package:home_ease/home/cubit/home_state.dart';
+import 'package:home_ease/theme/lighted_background.dart';
 import 'package:home_ease/widgets/bottom_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,53 +14,59 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: SHColors.backgroundColor,
-      appBar: AppBar(
-        backgroundColor: SHColors.backgroundColor,
-        title: Text(
-          "Home Ease",
-          style: Theme.of(context).textTheme.displayMedium,
+    return LightedBackgound(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(
+            "Home Ease",
+            style: Theme.of(context).textTheme.displayMedium,
+          ),
         ),
-      ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          Text(
-            "SELECT A ROOM",
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: Stack(
-              fit: StackFit.expand,
-              clipBehavior: Clip.none,
-              children: [
-                BlocBuilder<HomeCubit, HomeState>(
-                  builder: (context, state) {
-                    return RoomsPageView(
-                        controller: PageController(viewportFraction: 0.8));
-                  },
-                ),
-              ],
+        body: Column(
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              "SELECT A ROOM",
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
-          ),
-          const SizedBox(height: 20),
-          BlocBuilder<HomeCubit, HomeState>(
-            builder: (context, state) {
-              return PageIndicator(pageIndex: state.pageIndex, length: 5);
-            },
-          ),
-          BlocBuilder<HomeCubit, HomeState>(
-            builder: (context, state) {
-              return BottomNavBar(
-                currentIndex: state.selectedIndex,
-                onTap: (index) =>
-                    context.read<HomeCubit>().updateSelectedIndex(index),
-              );
-            },
-          ),
-        ],
+            const SizedBox(height: 20),
+            Expanded(
+              child: Stack(
+                fit: StackFit.expand,
+                clipBehavior: Clip.none,
+                children: [
+                  BlocBuilder<HomeCubit, HomeState>(
+                    builder: (context, state) {
+                      log("Page View ${state.pageIndex + 1}");
+                      return RoomsPageView(
+                        controller: PageController(viewportFraction: 0.8),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            BlocBuilder<HomeCubit, HomeState>(
+              builder: (context, state) {
+                log("Page Indicator ${state.pageIndex + 1}");
+                return PageIndicator(pageIndex: state.pageIndex, length: 5);
+              },
+            ),
+            BlocSelector<HomeCubit, HomeState, int>(
+              selector: (state) => state.selectedIndex,
+              builder: (context, selectedIndex) {
+                log("Bottom Nav");
+                return BottomNavBar(
+                  currentIndex: selectedIndex,
+                  onTap: (index) =>
+                      context.read<HomeCubit>().updateSelectedIndex(index),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/home/ui/rooms_page_view.dart
+++ lib/home/ui/rooms_page_view.dart
@@ -5,37 +5,78 @@ import 'package:home_ease/home/cubit/home_cubit.dart';
 class RoomsPageView extends StatelessWidget {
   final PageController controller;
 
-  const RoomsPageView({super.key, required this.controller});
+  final roomNames = [
+    'LIVING ROOM',
+    'DINING ROOM',
+    'BED ROOM',
+    'KITCHEN',
+    'BATHROOM',
+  ];
+
+  final List<String> imagePaths = [
+    'assets/images/living_room.jpeg',
+    'assets/images/dining_room.jpeg',
+    'assets/images/bed_room.jpg',
+    'assets/images/kitchen.jpg',
+    'assets/images/bathroom.jpg',
+  ];
+
+  RoomsPageView({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
       controller: controller,
-      itemCount: 5,
+      itemCount: roomNames.length,
       onPageChanged: (index) {
         context.read<HomeCubit>().updatePageIndex(index.toDouble());
       },
       itemBuilder: (_, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 10,
-                  offset: Offset(0, 5),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Image.asset(
+                          imagePaths[index],
+                          fit: BoxFit.cover,
+                        ),
+                        Container(
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ],
-            ),
-            child: Center(
-              child: Text(
-                'Room ${index + 1}',
-                style: Theme.of(context).textTheme.bodyLarge,
               ),
-            ),
+              Positioned(
+                left: 10,
+                bottom: 0,
+                top: 0,
+                child: Center(
+                  child: RotatedBox(
+                    quarterTurns: -1,
+                    child: Text(
+                      roomNames[index],
+                      style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         );
       },
